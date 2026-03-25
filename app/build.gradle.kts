fe6a9f88a6544605d74119b5a93ab065f2eb5784
ponimay <cevrdovatana@gmail.com>
2026-03-25 15:11:44 +0300

    Day 8: Material Design styling for buttons"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.activity:activity-ktx:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
 }
